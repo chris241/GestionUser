ef59824a579ba7449744e26b3ee1da89a3cd2850
chris241 <chrisleon325@gmail.com>
2020-12-08 11:44:20 +0300

    ad

---
+++ GestionUserBack/GestionUserBack.Entity/Services/CreateUserReq.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionUserBack.Entity.Services
+{
+   public  class CreateUserReq
+    {
+        [JsonProperty("id")]
+        public virtual Guid Id { get; set; }
+        [JsonProperty("name")]
+        public virtual string Name { get; set; }
+
+        [JsonProperty("dateCreate")]
+        public virtual DateTime DateCreate { get; set; }
+
+        [JsonProperty("dateModify")]
+        public virtual DateTime DateModify { get; set; }
+
+        [JsonProperty("email")]
+        public virtual string Email { get; set; }
+    }
+}
